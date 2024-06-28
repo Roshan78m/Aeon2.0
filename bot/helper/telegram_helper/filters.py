5081590806873618322067fd81d1a1385f29e65f
Unknown-903 <shubhamsinghal05072008@gmail.com>
2024-06-28 12:13:53 +0530

    Add files via upload

--- bot/helper/telegram_helper/filters.py
+++ bot/helper/telegram_helper/filters.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from pyrogram.filters import create
 from pyrogram.enums import ChatType
 
@@ -18,7 +19,8 @@ class CustomFilters:
         user = message.from_user or message.sender_chat
         uid = user.id
         chat_id = message.chat.id
-        return bool(uid == OWNER_ID or (uid in user_data and (user_data[uid].get('is_auth', False) or user_data[uid].get('is_sudo', False))) or (chat_id in user_data and user_data[chat_id].get('is_auth', False)))
+        return bool(uid == OWNER_ID or (uid in user_data and (user_data[uid].get('is_auth', False) or
+                                                              user_data[uid].get('is_sudo', False))) or (chat_id in user_data and user_data[chat_id].get('is_auth', False)))
 
     authorized = create(authorized_user)
     
--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -1,14 +1,15 @@
+#!/usr/bin/env python3
 from traceback import format_exc
 from asyncio import sleep
 from aiofiles.os import remove as aioremove
-from random import choice
+from random import choice as rchoice
 from time import time
 from re import match as re_match
 
 from pyrogram.types import InputMediaPhoto
 from pyrogram.errors import ReplyMarkupInvalid, FloodWait, PeerIdInvalid, RPCError, UserNotParticipant, MessageNotModified, MessageEmpty, PhotoInvalidDimensions, WebpageCurlFailed, MediaEmpty
 
-from bot import DELETE_LINKS, IMAGES, LOGGER, bot_name, status_reply_dict, status_reply_dict_lock, Interval, bot, user, download_dict_lock
+from bot import config_dict, LOGGER, bot_name, status_reply_dict, status_reply_dict_lock, Interval, bot, user, download_dict_lock
 from bot.helper.ext_utils.bot_utils import get_readable_message, setInterval, sync_to_async, download_image_url
 from bot.helper.telegram_helper.button_build import ButtonMaker
 from bot.helper.ext_utils.exceptions import TgLinkException
@@ -18,8 +19,8 @@ async def sendMessage(message, text, buttons=None, photo=None):
     try:
         if photo:
             try:
-                if photo == 'Random':
-                    photo = choice(IMAGES)
+                if photo == 'IMAGES':
+                    photo = rchoice(config_dict['IMAGES'])
                 return await message.reply_photo(photo=photo, reply_to_message_id=message.id, caption=text, reply_markup=buttons, disable_notification=True)
             except IndexError:
                 pass
@@ -46,8 +47,8 @@ async def sendCustomMsg(chat_id, text, buttons=None, photo=None):
     try:
         if photo:
             try:
-                if photo == 'Random':
-                    photo = choice(IMAGES)
+                if photo == 'IMAGES':
+                    photo = rchoice(config_dict['IMAGES'])
                 return await bot.send_photo(chat_id=chat_id, photo=photo, caption=text, reply_markup=buttons, disable_notification=True)
             except IndexError:
                 pass
@@ -99,8 +100,8 @@ async def sendMultiMessage(chat_ids, text, buttons=None, photo=None):
         try:
             if photo:
                 try:
-                    if photo == 'Random':
-                        photo = choice(IMAGES)
+                    if photo == 'IMAGES':
+                        photo = rchoice(config_dict['IMAGES'])
                     sent = await bot.send_photo(chat_id=chat.id, photo=photo, caption=text, reply_markup=buttons, disable_notification=True)
                     msg_dict[chat.id] = sent
                     continue
@@ -128,10 +129,21 @@ async def sendMultiMessage(chat_ids, text, buttons=None, photo=None):
 async def editMessage(message, text, buttons=None, photo=None):
     try:
         if message.media:
-            if photo:
-                return await message.edit_media(InputMediaPhoto(photo, text), reply_markup=buttons)
-            return await message.edit_caption(caption=text, reply_markup=buttons)
-        await message.edit(text=text, disable_web_page_preview=True, reply_markup=buttons)
+            try:
+                if photo:
+                  try:
+                      if photo == 'IMAGES':
+                          photo = rchoice(config_dict['IMAGES'])
+                      return await message.edit_media(InputMediaPhoto(photo, text), reply_markup=buttons)
+                      return await message.edit_caption(caption=text, reply_markup=buttons)
+                      
+                  except IndexError:
+                      pass
+                await message.edit(text=text, disable_web_page_preview=True, reply_markup=buttons)
+        
+            except IndexError:
+                      pass          
+        return await message.edit(text=text, disable_web_page_preview=True, reply_markup=buttons)        
     except FloodWait as f:
         LOGGER.warning(str(f))
         await sleep(f.value * 1.2)
@@ -155,6 +167,21 @@ async def sendFile(message, file, caption=None, buttons=None):
         return str(e)
 
 
+async def sendRss(text):
+    try:
+        if user:
+            return await user.send_message(chat_id=config_dict['RSS_CHAT_ID'], text=text, disable_web_page_preview=True, disable_notification=True)
+        else:
+            return await bot.send_message(chat_id=config_dict['RSS_CHAT_ID'], text=text, disable_web_page_preview=True, disable_notification=True)
+    except FloodWait as f:
+        LOGGER.warning(str(f))
+        await sleep(f.value * 1.2)
+        return await sendRss(text)
+    except Exception as e:
+        LOGGER.error(str(e))
+        return str(e)
+
+
 async def deleteMessage(message):
     try:
         await message.delete()
@@ -170,11 +197,11 @@ async def five_minute_del(message):
     await deleteMessage(message)
 
 async def delete_links(message):
-    if DELETE_LINKS:
-        if reply_to := message.reply_to_message:
-            await deleteMessage(reply_to)
-        await deleteMessage(message)
-
+    if reply_to := message.reply_to_message:
+        await deleteMessage(reply_to)
+    await deleteMessage(message)
+        
+        
 async def delete_all_messages():
     async with status_reply_dict_lock:
         for key, data in list(status_reply_dict.items()):
@@ -224,7 +251,8 @@ async def get_tg_link_content(link):
     elif not private:
         return message, 'bot'
     else:
-        raise TgLinkException("Bot can't download from GROUPS without joining!")
+        raise TgLinkException(
+            "Bot can't download from GROUPS without joining!")
 
 
 async def update_all_messages(force=False):
@@ -286,7 +314,7 @@ async def forcesub(message, ids, button=None):
     if join_button:
         if button is None:
             button = ButtonMaker()
-        _msg = "You haven't joined our channel/group yet!"
+        _msg = "You haven't joined our channel yet!"
         for key, value in join_button.items():
             button.ubutton(f'Join {key}', value, 'footer')
     return _msg, button
@@ -306,5 +334,5 @@ async def BotPm_check(message, button=None):
         if button is None:
             button = ButtonMaker()
         _msg = "You haven't initiated the bot in a private message!"
-        button.ibutton("Start", f"aeon {user_id} private", 'header')
+        button.ibutton("Start", f"aeon {user_id} pmc", 'header')
         return _msg, button
